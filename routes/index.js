a01fbf139a669270a138928d8c8d4bc0e9702bc2
Ankit Raj <ankitatiiitr@gmail.com>
2021-01-27 14:46:25 +0530

    refactored auth for more security

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,61 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const auth = require("../util/auth.js");
-const mongoose = require("mongoose");
 const User = require("../models/user.model");
+const axios = require('axios');
 
 router.get("/", function (req, res, next) {
   res.json({ message: "Hi" });
 });
 
-router.post("/auth", (req, res, next) => {
-  User.findOne({ email: req.body.email })
+router.get("/auth", async (req, res, next) => {
+  const params = new URLSearchParams();
+  params.append("code", req.query.code);
+  params.append("client_id", process.env.CLIENT_ID);
+  params.append("client_secret", process.env.CLIENT_SECRET);
+  params.append("redirect_uri", process.env.REDIRECT_URI);
+  params.append("grant_type", "authorization_code");
+  
+  try{
+    const result = await axios.post("https://oauth2.googleapis.com/token", params, {headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+    const jwtTokenFromGoogle = result.data.id_token;
+    const userData = jwt.decode(jwtTokenFromGoogle);
+    
+    const {given_name, picture, email} = userData;
+    User.findOne({ email })
     .then((result) => {
       if (result) {
         const accessToken = jwt.sign(
-          { name: req.body.name, email: req.body.email },
+          { name: given_name, email: email },
           process.env.JWT_SECRET,
           { expiresIn: "30d" }
         );
-        res
+        return res
           .status(200)
           .json({
             message: "OK",
             task: "signedIn",
             token: accessToken,
-            result: result,
+            email,
+            given_name,
+            picture
           });
-        return;
       } else {
-        const user = new User({ email: req.body.email, name: req.body.name });
+        const user = new User({ email, given_name });
         user
           .save()
           .then(() => {
             const accessToken = jwt.sign(
               {
-                name: req.body.name,
-                email: req.body.email,
+                name: given_name,
+                email: email,
               },
               process.env.JWT_SECRET,
               { expiresIn: "30d" }
             );
             res
               .status(200)
-              .json({ message: "OK", task: "signedUp", token: accessToken });
+              .json({ message: "OK", task: "signedUp", token: accessToken, given_name, email, picture });
           })
           .catch((err) => {
             console.log(err);
@@ -59,6 +73,13 @@ router.post("/auth", (req, res, next) => {
       });
       return;
     });
+  }catch(err){
+    console.log(err);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+  
 });
 
 router.get("/done", auth, (req, res, next) => {
